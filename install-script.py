1f77a46ae4331e738be5d5a32e3cf43e15a4cac8
Benegamer <Benedikt.kasten@gmail.com>
2021-04-03 13:18:57 +0200

    Merged into main2.0.py
    Removed config.py
    Removed debug.py
    Removed main.py
    Removed README.md
    Removed setup.py

--- install-script.py
+++ install-script.py
@@ -1,4 +1,22 @@
-#Run this scipt to install all the needed modules
-#
-#
-#
+#Setup script
+import json
+import sys
+
+def write_json():
+    # Data to be written
+    dictionary = {
+        "token": "Put your Token here!",
+        "prefix": "Put your Prefix here!",
+        "admin_id": "Put the Admin ID here!"
+    }
+
+    # Serializing json
+    json_object = json.dumps(dictionary, indent=3)
+
+    # Writing to sample.json
+    with open("config.json", "w") as outfile:
+        outfile.write(json_object)
+
+print("Setup complete! I did:")
+print(f"Wrote Json" + write_json())
+sys.exit(1)
---
+++ main2.0.py
@@ -0,0 +1,109 @@
+#Imports
+import datetime
+import discord
+from discord.ext import commands
+from discord.utils import get
+import json
+
+
+#Va
+token = 'null'
+bot_prefix = '!'
+admin_id ='000000000000000000'
+
+default_game = "!helpme for personal help"
+#read json for token prefix and adminid
+def read_json():
+    try:
+        # Opening JSON file
+        with open('config.json', 'r') as openfile:
+            # Reading from json file
+            data = json.load(openfile)
+
+            token = (data['token'])
+
+            bot_prefix = (data['bot_prefix'])
+
+            admin_id = (data['admin_id'])
+    except:
+        print("Somethin went wrong! I can feel it! \n")
+
+
+#set env for Bot
+bot = commands.Bot(command_prefix=bot_prefix)
+bot_prefix = bot_prefix
+
+
+#embed for help
+embed = discord.Embed(
+    title="Hallo World",
+    color=0xe67e22,
+    description="Help Bot\n"
+                "Dies ist der !h befehl\n"
+)
+embed.set_author(
+    name="Benegamer",
+)
+embed.add_field(
+    name="Befehle",
+    value="Dies sind alle Befehle, das Prefix ist " + bot_prefix + "\n"
+          "...",
+    inline=False
+)
+embed.add_field(
+    name="Dieser Bot wurde von Benegamer geschrieben",
+    value="Bei Bugs ihn anschreiben !",
+    inline=False
+)
+
+@bot.event
+async def on_ready():
+    print('User connected:')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('---------------')
+    username = bot.user.name
+    botid = bot.user.id
+
+
+#Botcommands
+@bot.command(pass_context=True, aliases=['h'])
+async def helpme(ctx):
+    #debug.printme(f"Send help to {ctx.message.author}")
+    await ctx.send(embed=embed)
+
+
+@bot.command(pass_context=True, aliases=['t'])
+async def time(ctx):
+    await ctx.send(f"""Wir haben {datetime.datetime.now().time()} Uhr""")
+
+
+#Music
+@bot.command(pass_context = True, aliases=['j'])
+async def join(ctx):
+    global voice
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    try:
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else:
+            voice = await channel.connect()
+
+        await ctx.send(f"Joined {channel}")
+    except:
+        print(Exception)
+        await ctx.send(Exception + "Something is wrong I can feel it!")
+
+@bot.command(pass_context = True, aliases=['l'])
+async def leave(ctx):
+    try:
+        await ctx.voice_client.disconnect()
+    except:
+        await ctx.send(Exception + "Maybe Im not in a Channel? ")
+
+
+#Startup
+read_json()
+
+bot.run(token)
