c140add011a9da1473122669ec46ffe1b588bb8a
SoftwareStep <doom10121993@hotmail.com>
2021-04-24 18:00:48 +0200

    Added TOKEN and PREFIX to environment

--- build.bat
+++ build.bat
@@ -1,19 +1,21 @@
-if exist config.json (
+for /f "delims== tokens=1,2" %%G in (Config.txt) do set %%G=%%H
+
+if exist Config.txt (
 cls
 docker build -t discordbot .     
 
-docker run -d --name discordbot --restart unless-stopped redis discordbot
+docker run -d --name discordbot --env PREFIX=%PREFIX% --env TOKEN=%TOKEN% --restart unless-stopped redis discordbot
 pause
 ) else (
 
 cls
-echo YOU DONT HAVE THE CONFIG.JSON FILE IN THE CURRENT DIRECTORY!
-echo YOU DONT HAVE THE CONFIG.JSON FILE IN THE CURRENT DIRECTORY!
-echo YOU DONT HAVE THE CONFIG.JSON FILE IN THE CURRENT DIRECTORY!
-echo YOU DONT HAVE THE CONFIG.JSON FILE IN THE CURRENT DIRECTORY!
-echo YOU DONT HAVE THE CONFIG.JSON FILE IN THE CURRENT DIRECTORY!
-echo YOU DONT HAVE THE CONFIG.JSON FILE IN THE CURRENT DIRECTORY!
-echo TO DOWNLOAD THEM GO TO https://raw.githubusercontent.com/Benegamer/PyBot-Discord/master/config.json
+echo YOU DONT HAVE THE CONFIG.TXT FILE IN THE CURRENT DIRECTORY!
+echo YOU DONT HAVE THE CONFIG.TXT FILE IN THE CURRENT DIRECTORY!
+echo YOU DONT HAVE THE CONFIG.TXT FILE IN THE CURRENT DIRECTORY!
+echo YOU DONT HAVE THE CONFIG.TXT FILE IN THE CURRENT DIRECTORY!
+echo YOU DONT HAVE THE CONFIG.TXT FILE IN THE CURRENT DIRECTORY!
+echo YOU DONT HAVE THE CONFIG.TXT FILE IN THE CURRENT DIRECTORY!
+echo TO DOWNLOAD THEM GO TO https://raw.githubusercontent.com/Benegamer/PyBot-Discord/master/Config.txt
 pause
 
 )
